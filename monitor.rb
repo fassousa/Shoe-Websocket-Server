ee4a80fa2b0c3e2605a5e5a25ea9533c8158f28e
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2022-06-22 06:38:28 -0300

    Create basic logger

---
+++ monitor.rb
@@ -0,0 +1,21 @@
+require 'faye/websocket'
+require 'eventmachine'
+require 'json'
+require 'byebug'
+
+def monitor(data)
+  store_name = data['store']
+  store_name = store_name.gsub("ALDO", '')
+
+  return "ATTENTION -#{store_name} inventory is almost full, with #{data['inventory']}!" if data['inventory'] > 80
+  return "WARNING -#{store_name} inventory is almost empty, with #{data['inventory']}!" if data['inventory'] < 20
+  return "INFO -#{store_name} inventory is ok, with #{data['inventory']}!"
+end
+
+EM.run {
+  ws = Faye::WebSocket::Client.new('ws://localhost:8080/')
+
+  ws.on :message do |event|
+    p monitor(JSON.parse(event.data))
+  end
+}
